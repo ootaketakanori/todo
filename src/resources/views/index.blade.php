ab59a4c3d039ac4305f2d893eac41b293beecf15
ootake takanori <mine19876293@gmail.com>
2023-12-03 17:34:32 +0900

    1-3

--- src/resources/views/index.blade.php
+++ src/resources/views/index.blade.php
@@ -78,3 +78,5 @@
         </div>
         @endsection
     </main>
+
+</html>
--- src/resources/views/layouts/app.blade.php
+++ src/resources/views/layouts/app.blade.php
@@ -1,11 +1,11 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="ja">
 
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Todo</title>
     <link rel="stylesheet" href="{{ asset('css/sanitize.css') }}">
     <link rel="stylesheet" href="{{ asset('css/common.css') }}">
     @yield('css')
@@ -14,7 +14,7 @@
 <body>
     <header class="header">
         <div class="header__inner">
-            <a class="header__logo">
+            <a class="header__logo" href="/">
                 Todo
             </a>
         </div>
