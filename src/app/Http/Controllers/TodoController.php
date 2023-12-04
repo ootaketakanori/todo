d1075dd1c92cca0dcaba215188f01e130aaa0dbb
ootake takanori <mine19876293@gmail.com>
2023-12-04 20:55:56 +0900

    1-4

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -10,13 +10,13 @@ class TodoController extends Controller
     public function index()
     {
         $todos = Todo::all();
+
         return view('index', compact('todos'));
     }
     public function store(Request $request)
     {
         $todo = $request->only(['content']);
         Todo::create($todo);
-
 
         return redirect('/');
     }
